bb8dec37132e3a862d73b5778eb0f1f7eda6f757
Sweta7 <anvita1982@gmail.com>
2025-07-21 13:22:39 +1000

    added server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://0z1jj2nj-3000.aue.devtunnels.ms/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
